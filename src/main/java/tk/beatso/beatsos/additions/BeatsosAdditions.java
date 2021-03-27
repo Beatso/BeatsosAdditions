ad4ac1a41398aaf8cd8700085f102f1f92e690a1
Beatso <39217836+Beatso@users.noreply.github.com>
2021-03-27 01:21:40 +0000

    update package name

--- src/main/java/tk/beatso/beatsos/additions/BeatsosAdditions.java
+++ src/main/java/tk/beatso/beatsos/additions/BeatsosAdditions.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package tk.beatso.beatsos.additions;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class BeatsosAdditions implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
--- src/main/java/tk/beatso/beatsos/additions/mixin/ExampleMixin.java
+++ src/main/java/tk/beatso/beatsos/additions/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package tk.beatso.beatsos.additions.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
