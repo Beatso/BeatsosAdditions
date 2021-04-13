f2e7e68db61057da3b475e94308708c21f67a88a
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-13 10:47:37 +0100

    add sturdiness potion

--- src/main/java/tk/beatso/beatsos/additions/statuseffect/BeatsosAdditionsStatusEffects.java
+++ src/main/java/tk/beatso/beatsos/additions/statuseffect/BeatsosAdditionsStatusEffects.java
@@ -1,5 +1,7 @@
 package tk.beatso.beatsos.additions.statuseffect;
 
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import tk.beatso.beatsos.additions.statuseffect.effects.SturdinessStatusEffect;
@@ -9,6 +11,8 @@ public class BeatsosAdditionsStatusEffects {
 	public static final void effects() {
 		
 		Registry.register(Registry.STATUS_EFFECT, new Identifier("beatsosadditions", "sturdiness"), SturdinessStatusEffect.STURDINESS);
+
+		Registry.register(Registry.POTION, new Identifier("beatsosadditions", "sturdiness"), new Potion(new StatusEffectInstance(SturdinessStatusEffect.STURDINESS, 3600)));
 
 	}
 
