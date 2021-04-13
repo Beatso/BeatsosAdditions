bb234c03873e9efd1d7a930ca0fd7b28f8b27398
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-13 16:57:41 +0100

    sturdiness makes the player immune to soul sucking

--- src/main/java/tk/beatso/beatsos/additions/block/blocks/SoulStoneBlock.java
+++ src/main/java/tk/beatso/beatsos/additions/block/blocks/SoulStoneBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import tk.beatso.beatsos.additions.statuseffect.effects.SturdinessStatusEffect;
 
 public class SoulStoneBlock extends Block {
 
@@ -22,7 +23,7 @@ public class SoulStoneBlock extends Block {
 
 			LivingEntity livingEntity = ((LivingEntity) entity);
 
-			if (livingEntity.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH) > 2.0D) {
+			if (livingEntity.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH) > 2.0D && !livingEntity.hasStatusEffect(SturdinessStatusEffect.STURDINESS)) {
 
 				livingEntity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).addPersistentModifier(new EntityAttributeModifier("1146f672-aaa6-4c70-b601-5c99d0a00efd", -2.0D, EntityAttributeModifier.Operation.ADDITION));
 
