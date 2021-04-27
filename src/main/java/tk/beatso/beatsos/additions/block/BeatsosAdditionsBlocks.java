518dbc43e851fb6f71db0f4a402cadced63a571b
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-27 20:11:20 +0100

    remove comments

--- src/main/java/tk/beatso/beatsos/additions/block/BeatsosAdditionsBlocks.java
+++ src/main/java/tk/beatso/beatsos/additions/block/BeatsosAdditionsBlocks.java
@@ -7,18 +7,12 @@ import tk.beatso.beatsos.additions.block.blocks.SoulStoneBlock;
 
 public class BeatsosAdditionsBlocks {
 
-	// public static final SmoothStoneStairsBlock SMOOTH_STONE_STAIRS = new SmoothStoneStairsBlock(Blocks.SMOOTH_STONE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.SMOOTH_STONE));
-	// public static final SoulStoneBlock SOUL_STONE = new SoulStoneBlock();
-
 	public static final IronLadderBlock IRON_LADDER = new IronLadderBlock();
 	public static final SmoothStoneStairsBlock SMOOTH_STONE_STAIRS = new SmoothStoneStairsBlock();
 	public static final SoulJackOLanternBlock SOUL_JACK_O_LANTERN = new SoulJackOLanternBlock();
 	public static final SoulStoneBlock SOUL_STONE = new SoulStoneBlock();
 
 	public static final void blocks() {
-
-		// Registry.register(Registry.BLOCK, new Identifier(BeatsosAdditions.MOD_ID, "smooth_stone_stairs"), SMOOTH_STONE_STAIRS);
-		// Registry.register(Registry.ITEM, new Identifier(BeatsosAdditions.MOD_ID, "smooth_stone_stairs"), new BlockItem(SMOOTH_STONE_STAIRS, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		IronLadderBlock.register(IRON_LADDER);
 		SmoothStoneStairsBlock.register(SMOOTH_STONE_STAIRS);
