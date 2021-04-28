6aa7bd8e3709ec6835b30067e313b810b316f77b
Beatso <39217836+Beatso@users.noreply.github.com>
2021-04-28 18:17:29 +0100

    new statuseffect layout

--- src/main/java/tk/beatso/beatsos/additions/statuseffect/BeatsosAdditionsStatusEffects.java
+++ src/main/java/tk/beatso/beatsos/additions/statuseffect/BeatsosAdditionsStatusEffects.java
@@ -1,25 +1,22 @@
 package tk.beatso.beatsos.additions.statuseffect;
 
-import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.potion.Potion;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import tk.beatso.beatsos.additions.BeatsosAdditions;
 import tk.beatso.beatsos.additions.statuseffect.effects.SturdinessStatusEffect;
 
 public class BeatsosAdditionsStatusEffects {
 
-	public static final Potion STURDINESS_POTION = new Potion(new StatusEffectInstance(SturdinessStatusEffect.STURDINESS, 3600));
-	public static final Potion STURDINESS_POTION_LONG = new Potion(new StatusEffectInstance(SturdinessStatusEffect.STURDINESS, 9600));
-	public static final Potion STURDINESS_POTION_STRONG = new Potion(new StatusEffectInstance(SturdinessStatusEffect.STURDINESS, 1800, 1));
+	public static StatusEffect STURDINESS;
+	public static Potion STURDINESS_POTION;
+	public static Potion STURDINESS_POTION_LONG;
+	public static Potion STURDINESS_POTION_STRONG;
 
 	public static final void effects() {
 		
-		Registry.register(Registry.STATUS_EFFECT, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness"), SturdinessStatusEffect.STURDINESS);
-
-		Registry.register(Registry.POTION, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness"), STURDINESS_POTION);
-		Registry.register(Registry.POTION, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness_long"), STURDINESS_POTION_LONG);
-		Registry.register(Registry.POTION, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness_strong"), STURDINESS_POTION_STRONG);
+		STURDINESS = SturdinessStatusEffect.RegisterStatusEffect();
+		STURDINESS_POTION = SturdinessStatusEffect.RegisterPotionNormal();
+		STURDINESS_POTION_LONG = SturdinessStatusEffect.RegisterPotionLong();
+		STURDINESS_POTION_STRONG = SturdinessStatusEffect.RegisterPotionStrong();
 
 	}
 
--- src/main/java/tk/beatso/beatsos/additions/statuseffect/effects/SturdinessStatusEffect.java
+++ src/main/java/tk/beatso/beatsos/additions/statuseffect/effects/SturdinessStatusEffect.java
@@ -3,7 +3,12 @@ package tk.beatso.beatsos.additions.statuseffect.effects;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.potion.Potion;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import tk.beatso.beatsos.additions.BeatsosAdditions;
 
 public class SturdinessStatusEffect extends StatusEffect {
 
@@ -14,7 +19,21 @@ public class SturdinessStatusEffect extends StatusEffect {
 		);
 	}
 
+	public static StatusEffect RegisterStatusEffect() {
+		return Registry.register(Registry.STATUS_EFFECT, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness"), SturdinessStatusEffect.STURDINESS);
+	}
+	public static Potion RegisterPotionNormal() {
+		return Registry.register(Registry.POTION, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness"), new Potion(new StatusEffectInstance(SturdinessStatusEffect.STURDINESS, 3600)));
+	}
+	public static Potion RegisterPotionLong() {
+		return Registry.register(Registry.POTION, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness_long"), new Potion(new StatusEffectInstance(SturdinessStatusEffect.STURDINESS, 9600)));
+	}
+	public static Potion RegisterPotionStrong() {
+		return Registry.register(Registry.POTION, new Identifier(BeatsosAdditions.MOD_ID, "sturdiness_strong"), new Potion(new StatusEffectInstance(SturdinessStatusEffect.STURDINESS, 1800, 1)));
+	}
+
 	public static final StatusEffect STURDINESS =
 		new SturdinessStatusEffect().addAttributeModifier(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, "D3F78FA6-D282-482B-A572-E17BD971597D", 0.20000000298023224D, EntityAttributeModifier.Operation.ADDITION);
+	
 
 }
